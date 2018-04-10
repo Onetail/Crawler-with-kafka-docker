81c6a5665695e6dac1f47aad6103ef8c1cc92d78
Onetail <paosong91@gmail.com>
2018-04-10 23:53:13 +0800

    have a bug do similarity

--- crawler/CosineSimilarity.py
+++ crawler/CosineSimilarity.py
@@ -1,8 +1,38 @@
 import jieba
 import jieba.analyse
+import numpy
+from sklearn import feature_extraction
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-def jiebaAnalyse():
-    jieba.set_dictionary('../example/dict.txt.big')
-    content = open('lyrics.txt', 'rb').read()
-    tags = jieba.analyse.extract_tags(content, 10, withWeight=True)
-    print(tags)
+def jiebaAnalyse(corpus=[""]):
+    jieba.set_dictionary('./example/dict.txt.big')
+    vectorizer = TfidfVectorizer()
+    tfidf = vectorizer.fit_transform(corpus)
+    fv = tfidf.toarray()
+    print(tfidf.shape)
+
+    articles = []
+    words = vectorizer.get_feature_names()
+    for i in range(len(corpus)):
+        print('----Document %d----' % (i+1))
+        for j in range(len(words)):
+            if tfidf[i,j] > 1e-5:
+                try:
+                    # print(words[j], tfidf[i,j])
+                    articles.append(cosine_similarity(tfidf[0],tfidf))
+                    # print(result)
+                    # print(fv[j])
+                except:
+                    pass
+    print(articles)
+    return articles,i+1
+        
+def comparisonSimilarity(articles,number):
+    pass
+    # line = []
+    # for i in range(number):
+    #     line[i] = numpy.array([])
+    #     for j in range(number):
+    #         if not j == i:
+    #             line[i][i,j] = cosine_similarity(articles[i],articles[j])
--- crawler/Crawler.py
+++ crawler/Crawler.py
@@ -1,33 +1,40 @@
 import requests
 import time
 import json
-from assets import Global
+import jieba
 from bs4 import BeautifulSoup
+from assets import Global
+from crawler import CosineSimilarity as cs
 
 def get_articles():
 	res = requests.get(Global.YAHOO_URL,headers = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'})
 	res.encoding = 'utf-8'
 	soup = BeautifulSoup(res.text,"html.parser")
 
-	articles = [] 
+	articles = []
 	#all_li = soup.select('div[id="YDC-Stream"] > ul > li') 
 	for li in soup.find_all('li',{'class':'js-stream-content Pos(r)'}):
 		if(len(li.div.div.find_all('span')) >= 2):
-			print(li.div.div.find_all('span')[0].text)
-			print(li.div.div.find_all('span')[1].text)
-			print(li.div.div.h3.find('a').text)   #標題
-			print(li.div.div.h3.find('a')['href'])
-			print('')
+			try:
+				print(li.div.div.find_all('span')[0].text)
+				print(li.div.div.find_all('span')[1].text)
+				print(li.div.div.h3.find('a').text)   #標題
+				print(li.div.div.h3.find('a')['href'])
+				print('')
+			except:
+				pass 
 			link = 'https://tw.news.yahoo.com'+str(li.div.div.h3.find('a')['href'])
 			co_res = requests.get(link,headers = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'})
 			co_res.encoding = 'utf8'
 			co_soup = BeautifulSoup(co_res.text,"html.parser")
 			if(co_soup.find('article',{'itemprop':'articleBody'})):
+				news_string = ""
 				for p in co_soup.find('article',{'itemprop':'articleBody'}).find_all('p'): #content
-					try:
-						print(p.text)
-					except:
-						print("\n.\n")
+					news_string += p.text
+				articles.append(" ".join(jieba.cut(news_string,cut_all=False)))
+				
+	articles_sim,articles_number = cs.jiebaAnalyse(articles)
+	cs.comparisonSimilarity(articles_sim,articles_number)
 
 
 
